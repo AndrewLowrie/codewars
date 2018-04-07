2ab3dbb9908f610ea14b0916bd926b16a5f63928
AndrewLowrie <andrew2lowrie@googlemail.com>
2018-04-07 20:29:44 +0100

    Add count of positives and sum of negatives solution

--- 8kyu/count_of_positives_sum_of_negatives.rb
+++ 8kyu/count_of_positives_sum_of_negatives.rb
@@ -23,6 +23,3 @@ def count_positives_sum_negatives(lst)
   result = [positive_count.count]
   return result.push(negative_sum)
 end
-
-count_positives_sum_negatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])
-#  10,-65])
